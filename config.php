272bbd57c823806f3c4a771cf16a30bcea8704e7
Yusuf Ismail Badat <82292764+RtasVdam@users.noreply.github.com>
2025-06-14 22:18:05 +0200

    Update config.php

--- config.php
+++ config.php
@@ -7,12 +7,13 @@ if (!defined('SITE_NAME')) {
     define('SITE_NAME', 'R3Booted Technology');
 }
 
-$host = getenv("MYSQLHOST") ?: 'localhost';
-$user = getenv("MYSQLUSER") ?: 'root';
-$pass = getenv("MYSQLPASSWORD") ?: '';
+$host = getenv("MYSQLHOST");
+$port = getenv("MYSQLPORT") ?: 3306;
+$user = getenv("MYSQLUSER");
+$pass = getenv("MYSQLPASSWORD");
 $db   = getenv("MYSQLDATABASE") ?: 'r3booted_ecommerce';
 
-$conn = new mysqli($host, $user, $pass, $db);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
